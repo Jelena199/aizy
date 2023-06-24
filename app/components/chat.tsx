b1fb3fcdda189ba3b84250b465c7959cb93b0e7e
santa-bringyouwishes <shaorun1010@gmail.com>
2023-06-24 19:01:09 +0900

    log speechrecognition

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -655,7 +655,7 @@ export function Chat() {
         setRecording(true);
         setSpeechRecognition();
         if (speechRecognition) {
-          onSpeechLog("speechrecognition true");
+          onSpeechLog(speechRecognition as unknown as string);
           speechRecognition.lang = "zh-CN";
           // speechRecognition.maxAlternatives = 5;
           speechRecognition.continuous = true;
